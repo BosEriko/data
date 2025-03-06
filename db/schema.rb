96525c68308e9fb4fe74ee988ec90ead09faa5a8
BosEriko <bos@eriko.ph>
2025-03-06 20:56:09 +0800

    Hotfix: Remove name from User Model

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_06_124033) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
