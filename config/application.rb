bd9c0ecc005ff2c919dda4fba10576b546d9d13b
BosEriko <bos@eriko.ph>
2025-03-07 15:43:40 +0800

    remove extra comma and add lines

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module BosErikoData
       # GraphQL-Ruby query log tags:
       current_graphql_operation: -> { GraphQL::Current.operation_name },
       current_graphql_field: -> { GraphQL::Current.field&.path },
-      current_dataloader_source: -> { GraphQL::Current.dataloader_source_class },
+      current_dataloader_source: -> { GraphQL::Current.dataloader_source_class }
     ]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
@@ -39,6 +39,6 @@ module BosErikoData
     config.api_only = true
 
     # Enable session middleware
-    config.middleware.use ActionDispatch::Session::CookieStore, key: '_bos_eriko_data_session'
+    config.middleware.use ActionDispatch::Session::CookieStore, key: "_bos_eriko_data_session"
   end
 end
