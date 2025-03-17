1eb22fd6e58d1177968c72a7aa6c6d20d3143f8a
BosEriko <bos@eriko.ph>
2025-03-17 11:24:11 +0800

    Feature: Return user_id to nil when it doesn't exist

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,7 +8,15 @@ class Member < ApplicationRecord
 
   validates :email, presence: true, uniqueness: { scope: :server_id, message: "should be unique within a server" }
 
+  before_save :nullify_invalid_user
+
   def admin?
     user_id.present?
+  end
+
+  private
+
+  def nullify_invalid_user
+    self.user_id = nil if user_id.present? && user.nil?
   end
 end
