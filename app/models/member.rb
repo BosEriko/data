49a45fef58e70315694e49e57017822054c15ed5
BosEriko <bos@eriko.ph>
2025-03-17 09:44:06 +0800

    Hotfix: Remove membership type

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,8 +1,4 @@
 class Member < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :server
-
-  enum :membership, { user: "user", admin: "admin" }
-
-  validates :membership, presence: true
 end
--- db/migrate/20250307091135_create_members.rb
+++ db/migrate/20250307091135_create_members.rb
@@ -3,7 +3,6 @@ class CreateMembers < ActiveRecord::Migration[8.0]
     create_table :members, id: :uuid do |t|
       t.references :user, type: :uuid, foreign_key: true, null: true
       t.references :server, type: :uuid, foreign_key: true, null: false
-      t.string :membership, null: false, default: "user"
       t.string :first_name
       t.string :last_name
       t.string :username
