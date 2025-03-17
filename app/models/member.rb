1a3272f20389a9654a332d0b3742efbab917f081
BosEriko <bos@eriko.ph>
2025-03-17 09:43:10 +0800

    Hotfix: Make user_id optional on Active Record level

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :server
 
   enum :membership, { user: "user", admin: "admin" }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,5 @@ class User < ApplicationRecord
   has_many :members
   has_many :servers, through: :members
 
-  has_many :admin_memberships, -> { where(membership_type: 'admin') }, class_name: 'Member'
-  has_many :administered_servers, through: :admin_memberships, source: :server
-
-  has_many :user_memberships, -> { where(membership_type: 'user') }, class_name: 'Member'
-  has_many :joined_servers, through: :user_memberships, source: :server
-
   validates :email, presence: true, uniqueness: true
 end
