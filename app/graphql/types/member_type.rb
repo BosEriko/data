dbbc5a552398cdf32d62304d7e557aa611719f99
BosEriko <bos@eriko.ph>
2025-04-09 07:38:10 +0800

    Hotfix: Remove serverId from member return

--- app/graphql/types/member_type.rb
+++ app/graphql/types/member_type.rb
@@ -4,7 +4,6 @@ module Types
   class MemberType < Types::BaseObject
     field :id, ID, null: false
     field :email, String, null: false
-    field :server_id, ID, null: false
     field :first_name, String, null: true
     field :last_name, String, null: true
     field :age, Integer, null: true
